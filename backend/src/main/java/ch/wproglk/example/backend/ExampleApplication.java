64b57fb907ae622a11eae0e47a5649504a1a28ee
Lukas Keller <lukas.keller@getabstract.com>
2023-09-08 13:59:41 +0200

    fix: rename group name

--- backend/src/main/java/ch/wproglk/example/backend/ExampleApplication.java
+++ backend/src/main/java/ch/wproglk/example/backend/ExampleApplication.java
@@ -1,13 +1,13 @@
-package ch.wprog.example.backend;
+package ch.wproglk.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class ExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(ExampleApplication.class, args);
 	}
 
 }
