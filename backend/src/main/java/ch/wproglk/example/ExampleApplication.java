ff821ed82d2e5299dfde9ac4640ba52d1babf9cc
Lukas Keller <lukas.keller@getabstract.com>
2023-09-25 16:59:05 +0200

    fix: remove necessary nested "backend" package

--- backend/src/main/java/ch/wproglk/example/ExampleApplication.java
+++ backend/src/main/java/ch/wproglk/example/ExampleApplication.java
@@ -1,9 +1,12 @@
-package ch.wproglk.example.backend;
+package ch.wproglk.example;
 
+import ch.wproglk.example.web.security.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class ExampleApplication {
 
 	public static void main(String[] args) {
